0a956b7578a3cebac125c8f6913972068d1f7f1d
Gian Carlo Orcotoma Mormontoy <giancarlozklm2015@gmail.com>
2024-08-30 18:26:21 -0500

    asdasd
    aaaa

--- buildfinder/settings.py
+++ buildfinder/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = "buildfinder.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_buildfinder',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'db_buildfinder',
+#         'USER': 'postgres',
+#         'PASSWORD': '1234',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
